12ba16221cadd668e1475d2d4f9ca3543ce23ac8
Nicholas Trujillo <nicholasrtrujillo@gmail.com>
2020-10-22 15:54:15 -0700

    hi

--- server/app.js
+++ server/app.js
@@ -11,13 +11,74 @@ const knex = require("knex")({
   },
 });
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/boxes", async (req, res) => {
-  const boxResult = await knex.raw("SELECT * FROM boxes");
-  const boxes = boxResult.rows;
-  res.json(boxes);
+app.get("/api/projects/:id", async (req, res) => {
+  const projectList = await knex.raw("SELECT * FROM projects WHERE id = ?", [
+    1,
+  ]);
+  const projects = projectList.rows;
+  res.json(projects);
+});
+
+app.get("/api/columns", async (req, res) => {
+  const columnList = await knex.raw("SELECT * FROM columns");
+  const columns = columnList.rows;
+  res.json(columns);
+});
+
+app.post("/api/columns", async (req, res) => {
+  const { title } = req.body;
+  await knex.raw("INSERT INTO columns (title, project_id) VALUES (?, ?)", [
+    title,
+    1,
+  ]);
+  res.json("created column");
+});
+
+app.patch("/api/columns/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  await knex.raw("UPDATE columns SET title = ? WHERE id = ?", [title, id]);
+  res.json("updated column");
+});
+
+app.delete("/api/columns/:id", async (req, res) => {
+  const { id } = req.params;
+  await knex.raw("DELETE FROM columns WHERE id = ?", [id]);
+  res.json("deleted column");
+});
+
+app.get("/api/cards", async (req, res) => {
+  const cardList = await knex.raw("SELECT * FROM cards");
+  const cards = cardList.rows;
+  res.json(cards);
+});
+
+app.post("/api/cards", async (req, res) => {
+  const { description } = req.body;
+  await knex.raw("INSERT INTO cards (description, column_id) VALUES (?, ?)", [
+    description,
+    column_id,
+  ]);
+  res.json("created card");
+});
+
+app.patch("/api/cards/:id", async (req, res) => {
+  const { id } = req.params;
+  const { description } = req.body;
+  await knex.raw("UPDATE columns SET title = ? WHERE id = ?", [
+    description,
+    id,
+  ]);
+  res.json("updated card");
+});
+
+app.delete("/api/cards/:id", async (req, res) => {
+  const { id } = req.params;
+  await knex.raw("DELETE FROM cards WHERE id = ?", [id]);
+  res.json("deleted card");
 });
 
 app.listen(3001, () => {
